c4bf32e9e57637d9854fdb02fe1634fe0f76f3af
Renat Galin <rensite@yandex.ru>
2022-12-15 22:04:35 +0500

    removes stylu in favor of scss

--- src/App.vue
+++ src/App.vue
@@ -8,79 +8,115 @@ header.header.content
       p.header-info-skill Front-End Dev / Designer
   .header-nav
     .header-nav-menu
-      a(href="#").menu-link Blog
-      a(href="#").menu-link GitHub
-      a(href="#").menu-link Projects
+      .header-nav-link contact me
     .header-nav-contacts
-      a(href="#").contact-link
-      a(href="#").contact-link
-      a(href="#").contact-link
-      a(href="#").contact-link
+      a(href="#").header-nav-social
+      a(href="#").header-nav-social
+      a(href="#").header-nav-social
+      a(href="#").header-nav-social
 RouterView
 </template>
 
-<style lang="stylus" scoped>
-  .header
-    display: flex
-    justify-content: space-between
-    align-items: flex-end
-    padding: 12px 0;
+<style lang="scss">
+  .header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+    padding: 1.8rem 0;
 
-    &-logo
-      display: flex
-      align-items: flex-end
-      gap: 20px
+    &-logo {
+      display: flex;
+      align-items: flex-end;
+      gap: 2rem;
+    }
     
-    &-avatar
-      width: 80px
-      height: 80px
-      overflow: hidden
-      border-radius: 50%
+    &-avatar {
+      width: 8rem;
+      height: 8rem;
+      overflow: hidden;
+      border-radius: 50%;
       border: 1px solid #eee;
+    }
+  }
   
-  .header-info
-      margin-bottom: 12px
-      
-      &-name
-        font-size: 32px
-        font-weight: 700
-        text-transform: uppercase
-        line-height: 1
-        margin: 0 0 5px
-        
-      &-skill
-        font-size: 19px
-        font-weight: 400
-        line-height: 1
-        margin: 0
-        
-    img
-      width: 100%
-      height: 100%
+  .header-info {
+    margin-bottom: 1.2rem;
     
-  .header-nav
-    display: grid
-    gap: 8px
-    margin-bottom: 14px
+    &-name {
+      font-size: 3.3rem;
+      font-weight: 700;
+      text-transform: uppercase;
+      line-height: 1;
+      margin: 0 0 .5rem;
+    }
+      
+    &-skill {
+      font-size: 1.9rem;
+      font-weight: 400;
+      line-height: 1;
+      margin: 0;
+    }
+  }
+    
+  .header-nav {
+    display: grid;
+    gap: .8rem;
+    margin-bottom: 1.4rem;
+    
+    @include mqPhones {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100vh;
+      z-index: 999;
+      background: #fff;
+      place-items: center;
+      grid-template-rows: 1fr auto;
+      padding-bottom: 24px;
+    }
       
     &-menu,
-    &-contacts
-      display: grid
-      grid-auto-flow: column
-      justify-content: right
-      gap: 12px
-    
-.contact-link
-  width: 24px
-  height: 24px
-  border-radius: 50%
-  background: #ccc
-  display: block
+    &-contacts {
+      display: grid;
+      grid-auto-flow: column;
+      justify-content: right;
+      gap: 1.2rem;
+    }
 
-a.menu-link
-  &, &:visited
-    text-decoration: none
-    color: #000
-    font-weight: 400
-    font-size: 17px
+    &-menu {
+      @include mqPhones {
+        grid-auto-flow: row;
+        height: max-content;
+        place-content: center;
+        text-align: center;
+      }
+    }
+
+    &-contacts {
+      @include mqPhones {
+        height: max-content;
+        place-content: center;
+      }
+    }
+
+    &-link {
+      &, &:visited {
+        text-decoration: none;
+        color: #000;
+        font-weight: 400;
+        font-size: 1.7rem;
+      }
+    }
+
+    &-social {
+      &, &:visited {
+        width: 2.4rem;
+        height: 2.4rem;
+        border-radius: 50%;
+        background: #ccc;
+        display: block;
+      }
+    }
+  }
 </style>
