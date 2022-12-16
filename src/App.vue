9988013c617e447085a96984443d10d00dc78492
Renat Galin <rensite@yandex.ru>
2022-12-16 20:33:11 +0500

    Adds social icons and links

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,28 @@ header.header.content
       h1.header-info-name Renat Galin 
       p.header-info-skill Front-End Dev / Designer
   .header-nav
-    .header-nav-close menu
+    .header-nav-close
+      p.header-info-skill menu
     .header-nav__layout
       .header-nav-menu
         .header-nav-link contact me
       .header-nav-contacts
-        a(href="#").header-nav-social
-        a(href="#").header-nav-social
-        a(href="#").header-nav-social
-        a(href="#").header-nav-social
+        a(href="https://t.me/renman" target="_blank").header-nav-social
+          IconTelegram
+        a(href="mailto:to@renatgalin.pro" target="_blank").header-nav-social
+          IconEmail
+        a(href="https://github.com/rensite" target="_blank").header-nav-social
+          IconGithub
 RouterView
 footer.footer.content
   .footer-copyright &copy; Renat Galin | Front-End Dev/Designer, 2022
 </template>
+
+<script setup>
+  import IconGithub from '@/components/icons/Github.vue'
+  import IconTelegram from '@/components/icons/Telegram.vue'
+  import IconEmail from '@/components/icons/Email.vue'
+</script>
 
 <style lang="scss">
   .header {
@@ -60,9 +69,11 @@ footer.footer.content
   
   .header-info {
     margin-bottom: 1.2rem;
+    color: var(--color-wet-asphalt);
 
     @include mqPhones {
       margin-bottom: .2rem;
+      margin-bottom: 0;
     }
     
     &-name {
@@ -74,7 +85,7 @@ footer.footer.content
 
       @include mqPhones {
         font-size: 2.2rem;
-        margin-bottom: 0;
+        margin: 0;
       }
     }
       
@@ -91,6 +102,7 @@ footer.footer.content
   }
     
   .header-nav {
+    position: relative;
 
     @include mqPhones {
       margin-bottom: 0;
@@ -122,29 +134,29 @@ footer.footer.content
 
       @include mqPhones {
         width: auto;
-        height: 3.3rem;
+        height: 3.45rem;
         display: flex;
         font-size: 1.3rem;
         line-height: 1;
         align-items: flex-end;
-        color: var(--color-text);
+        color: var(--color-wet-asphalt);
         
         &::before,
         &::after {
           content: '';
           width: 2.9rem;
           height: .4rem;
-          background-color: var(--color-text);
+          background-color: var(--color-wet-asphalt);
           position: absolute;
           right: 0;
         }
 
         &::before {
-          top: .3rem;
+          top: .55rem;
         }
 
         &::after {
-          top: 1.2rem;
+          top: 1.35rem;
         }
       }
     }
@@ -187,8 +199,8 @@ footer.footer.content
         width: 2.4rem;
         height: 2.4rem;
         border-radius: 50%;
-        background: #ccc;
         display: block;
+        color: var(--color-wet-asphalt);
       }
     }
   }
@@ -199,7 +211,7 @@ footer.footer.content
 
     &-copyright {
       font-size: 14px;
-      opacity: .5;
+      color: var(--color-silver);
     }
   }
 </style>
